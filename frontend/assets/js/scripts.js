25b81c68695eb012761ef7a47826c7a1a095c3dd
Tarik <topictarik82@gmail.com>
2025-03-23 20:26:45 +0100

    Fixed login and register routes

--- frontend/assets/js/scripts.js
+++ frontend/assets/js/scripts.js
@@ -10,7 +10,7 @@
 
 
             var app = $.spapp({
-                defaultView: "#charts2",
+                defaultView: "#explore",
                 templateDir: "./views/"
     
             });
