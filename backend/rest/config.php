1c5bb6c6269937fef2d6444d9869d4f2fa0a5c99
Tarik <topictarik82@gmail.com>
2025-06-08 21:23:40 +0200

    did i comit everything

--- backend/rest/config.php
+++ backend/rest/config.php
@@ -1,37 +1,24 @@
 <?php
-
-
-// Set the reporting
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL ^ (E_NOTICE | E_DEPRECATED));
-
-
-class Config
-{
-   public static function DB_NAME()
-   {
-       return 'comic_collection'; 
+class Config {
+   public static function DB_NAME() {
+       return Config::get_env("DB_NAME", "comic_collection");
    }
-   public static function DB_PORT()
-   {
-       return  3306;
+   public static function DB_PORT() {
+       return Config::get_env("DB_PORT", 3306);
    }
-   public static function DB_USER()
-   {
-       return 'root';
+   public static function DB_USER() {
+       return Config::get_env("DB_USER", 'root');
    }
-   public static function DB_PASSWORD()
-   {
-       return 'root';
+   public static function DB_PASSWORD() {
+       return Config::get_env("DB_PASSWORD", 'root');
    }
-   public static function DB_HOST()
-   {
-       return '127.0.0.1';
+   public static function DB_HOST() {
+       return Config::get_env("DB_HOST", '127.0.0.1');
    }
-
-
    public static function JWT_SECRET() {
-       return 'your_key_string';
+       return Config::get_env("JWT_SECRET", ',dpPL,Se%fM-UVQBwf/X0T&B!DF6%');
+   }
+   public static function get_env($name, $default){
+       return isset($_ENV[$name]) && trim($_ENV[$name]) != "" ? $_ENV[$name] : $default;
    }
 }
