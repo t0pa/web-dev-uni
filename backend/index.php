4a63e6352ac1d0ca5c1d524a9c26ad4c35d79170
Tarik <topictarik82@gmail.com>
2025-06-08 23:27:10 +0200

    selam

--- backend/index.php
+++ backend/index.php
@@ -68,16 +68,18 @@ require_once __DIR__ . '/rest/routes/ComicsRoutes.php';
 
 
 $allowedOrigins = [
-    "http://localhost/TarikTopic/web-dev-uni/backend/",
-   "https://comic-app-jhrcg.ondigitalocean.app/",
+    "http://localhost",
+    "https://comicfront-app-dul8l.ondigitalocean.app"
 ];
 
 if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
     header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
-} 
+    header("Access-Control-Allow-Credentials: true");  // only if needed
+}
 
 header("Access-Control-Allow-Methods: GET, POST, PATCH, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authentication");
+
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(204);
     exit();
