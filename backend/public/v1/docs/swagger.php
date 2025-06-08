db397385edaf1140f39c06f1c07486ed3207d668
Tarik <topictarik82@gmail.com>
2025-06-08 23:12:15 +0200

    cors error fix?

--- backend/public/v1/docs/swagger.php
+++ backend/public/v1/docs/swagger.php
@@ -8,7 +8,7 @@ require __DIR__ . '/../../../vendor/autoload.php';
 if($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1'){
     define('BASE_URL', 'http://localhost/TarikTopic/web-dev-uni/backend/');
 } else {
-    define('BASE_URL', 'https://add-production-server-after-deployment/backend/');
+    define('BASE_URL', 'https://comic-app-jhrcg.ondigitalocean.app/');
 }
 
 $openapi = \OpenApi\Generator::scan([
--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,5 +1,16 @@
 let Constants = {
-   PROJECT_BASE_URL: "http://localhost/TarikTopic/web-dev-uni/backend/",
+
+   get_api_base_url: function () {
+      if (location.hostname === "localhost" ){
+         return "http://localhost/TarikTopic/web-dev-uni/backend/";
+      }
+      else
+      {
+         return  "https://comic-app-jhrcg.ondigitalocean.app/"
+      }
+   },
+
+   //PROJECT_BASE_URL: "http://localhost/TarikTopic/web-dev-uni/backend/",
    USER_ROLE: "user",
    ADMIN_ROLE: "admin"
 }
