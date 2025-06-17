dea91532c7fd98c438bef2d00d0d7884ec197685
Tarik <topictarik82@gmail.com>
2025-06-18 01:51:13 +0200

    dep

--- backend/public/v1/docs/swagger.php
+++ backend/public/v1/docs/swagger.php
@@ -5,11 +5,12 @@ error_reporting(E_ALL);
 
 require __DIR__ . '/../../../vendor/autoload.php';
 
-/* if($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1'){
+ if($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1'){
     define('BASE_URL', 'http://localhost/TarikTopic/web-dev-uni/backend/');
 } else {
-} */
-    define('BASE_URL', 'https://comic-app-jhrcg.ondigitalocean.app/');
+ define('BASE_URL', 'https://comic-app-jhrcg.ondigitalocean.app/');
+} 
+    
 
 $openapi = \OpenApi\Generator::scan([
     __DIR__ . '/doc_setup.php',
