2a36af943608f4a60bb677896795eeb453f27552
Tarik <topictarik82@gmail.com>
2025-06-18 02:56:09 +0200

    swagger fix

--- backend/env_check.php
+++ backend/env_check.php
@@ -2,5 +2,5 @@
 echo "DB_HOST: " . getenv('DB_HOST') . PHP_EOL;
 echo "DB_PORT: " . getenv('DB_PORT') . PHP_EOL;
 echo "DB_USER: " . getenv('DB_USER') . PHP_EOL;
-echo "DB_PASSWORD: " . (getenv('DB_PASSWORD') ? '***set***' : 'not set') . PHP_EOL;
+echo "DB_PASSWORD: " . getenv('DB_PASSWORD')  . PHP_EOL;
 echo "DB_NAME: " . getenv('DB_NAME') . PHP_EOL;
